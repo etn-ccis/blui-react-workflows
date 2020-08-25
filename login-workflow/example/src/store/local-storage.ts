166ff7298ab4850a0afafcfa3d883572c1c0e253
joebochill <boyle.p.joseph@gmail.com>
2020-08-25 14:01:35 -0400

    Disable strict mode and lint the demo

--- login-workflow/example/src/store/local-storage.ts
+++ login-workflow/example/src/store/local-storage.ts
@@ -22,12 +22,6 @@ async function readAuthData(): Promise<AuthData> {
         email: userData.user,
         rememberMeData: rememberMeData,
     };
-    // return {
-    //     initialExpirationDate,
-    //     userId: userData.userId,
-    //     email: userData.user,
-    //     rememberMeData: rememberMeData,
-    // };
 }
 
 function saveAuthCredentials(user: string, userId: string): void {
