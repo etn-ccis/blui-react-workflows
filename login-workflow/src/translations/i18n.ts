7e90d252f65b79871ee5dce94ca30b97e80f4492
joebochill <boyle.p.joseph@gmail.com>
2021-04-27 08:22:19 -0400

    Add Spanish support

--- login-workflow/src/translations/i18n.ts
+++ login-workflow/src/translations/i18n.ts
@@ -7,12 +7,20 @@ void i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources: {
+            // English
             en: translations.english,
+            // French
             fr: translations.french,
             fr_US: translations.french,
             fr_CA: translations.french,
+            fr_FR: translations.french,
+            // Spanish
+            es: translations.spanish,
+            es_US: translations.spanish,
+            es_MX: translations.spanish,
+            es_ES: translations.spanish,
         },
-        lng: 'en', // TODO get the device locale for the default
+        lng: 'es', // TODO get the device locale for the default
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false,
