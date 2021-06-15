506926648a1a4ba678db03d504c5520b7b3732c3
daileytj <daileytj89@gmail.com>
2021-06-15 10:45:52 -0400

    Update jest config

--- login-workflow/jest.config.js
+++ login-workflow/jest.config.js
@@ -3,6 +3,9 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 module.exports = {
     transform: {
         ...tsjPreset.transform,
+        "\\.[jt]sx?$": "babel-jest",
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/mocks/fileTransformer.js',
     },
     globals: {
         'ts-jest': {
