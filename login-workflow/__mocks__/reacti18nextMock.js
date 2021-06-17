b51ac4de367739e2970cd40a2a9924247e13dfdc
daileytj <daileytj89@gmail.com>
2021-06-17 09:14:41 -0400

    Fix coverage reports, add i18n mock

---
+++ login-workflow/__mocks__/reacti18nextMock.js
@@ -0,0 +1,20 @@
+const reactI18Next = jest.createMockFromModule('react-i18next');
+
+reactI18Next.useTranslation = () => {
+  return {
+    t: (str) => str,
+    i18n: {
+      changeLanguage: () => new Promise(() => { }),
+    },
+  };
+};
+
+reactI18Next.Trans = () => Component => props => (
+  <Component t={() => ''} {...props} >
+    {props.children}
+  </Component>
+)
+
+module.exports = reactI18Next;
+
+export default {};
