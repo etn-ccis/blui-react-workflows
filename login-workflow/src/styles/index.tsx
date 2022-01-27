6180be6ba9077db775b71f3111d7c6bc8bd511ff
Suraj Karambe <bkarambe@gmail.com>
2022-01-28 02:08:49 +0530

    Reverted createStyles default import

--- login-workflow/src/styles/index.tsx
+++ login-workflow/src/styles/index.tsx
@@ -1,4 +1,4 @@
-import createStyles from '@material-ui/core/styles/createStyles';
+import { createStyles } from '@material-ui/core';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 
