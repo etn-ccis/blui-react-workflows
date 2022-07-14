f7d72e44953b0d81c6da44e45d0f83c7efee5d7f
jeffvg <jeffvgreiner@eaton.com>
2022-07-14 15:56:28 -0400

    prettier

--- login-workflow/example/cypress/e2e/login.spec.cy.ts
+++ login-workflow/example/cypress/e2e/login.spec.cy.ts
@@ -1,23 +1,21 @@
 /// <reference types="cypress" />
 
 describe('login screen', () => {
-  beforeEach(() => {
-      cy.visit('http://localhost:3000/custom-login-route');
-  });
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/custom-login-route');
+    });
 
-  it('should authenticate with valid id', () => {
-    cy.get('#email').click().type('test@test.com')
-    cy.get('#password').click().type('Test321!')
-    cy.contains('Log In').should('be.enabled').click();
-    cy.contains('Edit src');
+    it('should authenticate with valid id', () => {
+        cy.get('#email').click().type('test@test.com');
+        cy.get('#password').click().type('Test321!');
+        cy.contains('Log In').should('be.enabled').click();
+        cy.contains('Edit src');
+    });
 
-  });
-
-  it('should not authenticate with invalid id', () => {
-    cy.get('#email').click().type('test.com')
-    cy.get('#password').click()
-    cy.get('#email-helper-text').should('contain', 'Please enter a valid email')
-    cy.contains('Log In').should('be.disabled')
-
-  });
+    it('should not authenticate with invalid id', () => {
+        cy.get('#email').click().type('test.com');
+        cy.get('#password').click();
+        cy.get('#email-helper-text').should('contain', 'Please enter a valid email');
+        cy.contains('Log In').should('be.disabled');
+    });
 });
--- login-workflow/example/cypress/support/e2e.ts
+++ login-workflow/example/cypress/support/e2e.ts
@@ -14,10 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
 // cypress code coverage
-import '@cypress/code-coverage/support'
+import '@cypress/code-coverage/support';
