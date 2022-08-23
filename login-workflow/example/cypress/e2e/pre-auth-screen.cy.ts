5a10a57ebb0cc8b34ada2517bb0f4eae6e5d8e50
jeffvg <jeffvgreiner@eaton.com>
2022-08-23 14:35:14 -0400

    prettier

--- login-workflow/example/cypress/e2e/pre-auth-screen.cy.ts
+++ login-workflow/example/cypress/e2e/pre-auth-screen.cy.ts
@@ -6,18 +6,17 @@ describe('pre-auth guard route', () => {
     });
 
     it('should display guarded route page', () => {
-        cy.contains('example pre-auth')
+        cy.contains('example pre-auth');
     });
 
     it('should have status code 200', () => {
-        cy.request('http://localhost:3000/pre-auth')
-        .then((resp) => {
-            expect(resp.status).to.eq(200)})
+        cy.request('http://localhost:3000/pre-auth').then((resp) => {
+            expect(resp.status).to.eq(200);
+        });
     });
 
     it('should guard navigate to home page', () => {
         cy.contains('Navigate to Home').click();
-        cy.url()
-      .should('be.equal', 'http://localhost:3000/custom-login-route')
+        cy.url().should('be.equal', 'http://localhost:3000/custom-login-route');
     });
 });
