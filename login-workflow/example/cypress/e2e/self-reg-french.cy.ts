70541b925c1bb5b5282fda76a9ee6bd00ea8aeeb
jeffvg <jeffvgreiner@eaton.com>
2022-12-09 11:57:09 -0500

    update self reg french test

--- login-workflow/example/cypress/e2e/self-reg-french.cy.ts
+++ login-workflow/example/cypress/e2e/self-reg-french.cy.ts
@@ -8,28 +8,28 @@ describe('Language spanish', () => {
 
     it('should display self register in french', () => {
         cy.contains('Change Language-FR').click();
-        cy.contains('maintenant').click();
-        cy.get('.MuiCardHeader-root').should('contain', 'Accord de Licence');
+        cy.contains('Créer un compte').click();
+        cy.contains('Accord de Licence');
         cy.contains('accepte les conditions');
         cy.get('[type="checkbox"]').check();
         cy.contains('Prochain').should('be.enabled').click();
-        cy.get('.MuiCardHeader-root').should('contain', 'Créer un compte');
+        cy.contains('Créer un compte');
         cy.get('#email-label').should('contain', 'Adresse e-mail');
         cy.get('#email').type('test@test.com');
         cy.contains('Prochain').should('be.enabled').click();
-        cy.get('.MuiCardHeader-root').should('contain', 'Vérifier les courriels');
+        cy.contains('Vérifier les courriels');
         cy.get('#code-label').should('contain', 'Code de vérification');
         cy.get('#code').click().type('123');
         cy.contains('Renvoyer').should('be.enabled').click();
         cy.contains('Prochain').should('be.enabled').click();
-        cy.get('.MuiCardHeader-root').should('contain', 'Créer un Mot de Passe');
+        cy.contains('Créer un Mot de Passe');
         cy.get('#password').click().type('Test321!');
         cy.get('#confirm').click().type('Test321!');
         cy.contains('Prochain').should('be.enabled').click();
-        cy.get('.MuiCardHeader-root').should('contain', 'Détails du Compte');
+        cy.contains('Détails du Compte');
         cy.get('#first').click().type('fName');
         cy.get('#last').click().type('lName');
         cy.contains('Prochain').should('be.enabled').click();
-        cy.get('.MuiCardHeader-root').should('contain', 'Compte Créé!');
+        cy.contains('Compte Créé!');
     });
 });
