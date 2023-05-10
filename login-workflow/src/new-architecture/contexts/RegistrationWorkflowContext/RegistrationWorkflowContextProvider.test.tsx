3abb97894ee02efe837a6dfa54e0de3e4d694d3b
Suraj Karambe <surajakarambe@eaton.com>
2023-05-10 18:11:00 +0530

    Added missing return types to function

--- login-workflow/src/new-architecture/contexts/RegistrationWorkflowContext/RegistrationWorkflowContextProvider.test.tsx
+++ login-workflow/src/new-architecture/contexts/RegistrationWorkflowContext/RegistrationWorkflowContextProvider.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react';
-import { renderHook } from '@testing-library/react';
+import { render, screen, cleanup, renderHook } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { RegistrationWorkflowContextProps, useRegistrationWorkflowContext } from '.';
 import { RegistrationWorkflowContextProvider } from './provider';
@@ -32,26 +31,32 @@ describe('RegistrationWorkflowContext', () => {
         let values: {
             result: { current: RegistrationWorkflowContextProps };
         };
-        const CustomFlow = () => {
-            const Screen1 = () => {
-                values = renderHook(() => useRegistrationWorkflowContext());
+
+        const RegisterComponent: React.FC<React.PropsWithChildren<any>> = () => (
+            <RegistrationWorkflowContextProvider {...defaultProps} />
+        );
+
+        const CustomFlow: React.FC = () => {
+            const Screen1: React.FC = () => {
+                // eslint-disable-next-line
+                values = renderHook((): RegistrationWorkflowContextProps => useRegistrationWorkflowContext());
                 return <Typography>Screen 1</Typography>;
             };
 
             return (
-                <RegisterComp>
+                <RegisterComponent>
                     <Screen1 />
-                </RegisterComp>
+                </RegisterComponent>
             );
         };
 
-        const RegisterComp: React.FC<React.PropsWithChildren<any>> = () => (
-            <RegistrationWorkflowContextProvider {...defaultProps} />
-        );
         render(<CustomFlow />);
 
-        await (() => expect(values.result.current.currentScreen).toBe(0));
-        await (() => expect(values.result.current.totalScreens).toBe(2));
-        await (() => expect(values.result.current.screenData['Eula'].accepted).toBeTruthy());
+        // eslint-disable-next-line
+        await ((): void => expect(values.result.current.currentScreen).toBe(0));
+        // eslint-disable-next-line
+        await ((): void => expect(values.result.current.totalScreens).toBe(2));
+        // eslint-disable-next-line
+        await ((): void => expect(values.result.current.screenData['Eula'].accepted).toBeTruthy());
     });
 });
