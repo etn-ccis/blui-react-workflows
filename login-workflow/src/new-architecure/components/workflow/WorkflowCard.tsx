9dd09f77fdab90b18957d93a10f943c68132ec3c
Manoj1907 <manojmanu1907@gmail.com>
2023-05-11 20:15:54 +0530

    customisation-classes-code-changes

--- login-workflow/src/new-architecure/components/workflow/WorkflowCard.tsx
+++ login-workflow/src/new-architecure/components/workflow/WorkflowCard.tsx
@@ -1,59 +1,71 @@
-import React from 'react';
-import Box from '@mui/material/Box';
+import React, { ReactNode } from 'react';
+import Box, { BoxProps } from '@mui/material/Box';
 import { WorkflowCardActionsProps } from './WorkflowCardActions';
 import { WorkflowCardHeaderProps } from './WorkflowCardHeader';
 import { WorkflowCardInstructionProps } from './WorkflowCardInstructions';
-import { useTheme } from '@mui/material/styles';
+import { SxProps, Theme, useTheme } from '@mui/material/styles';
 import { useInjectedUIContext } from '../../../auth-shared';
 import defaultBackgroundImage from '../../../assets/images/background.svg';
-import Card, { CardProps } from '@mui/material/Card';
+import Card, { CardProps as CardPropsType } from '@mui/material/Card';
+import {
+    BrandedCardContainerClasses,
+    BrandedCardContainerClassKey,
+    getBrandedCardContainerUtilityClass,
+} from '../../../components/BrandedCardContainer/BrandedCardContainerClasses';
+import { Spinner, SpinnerProps } from '../../../components/Spinner';
+import { unstable_composeClasses as composeClasses } from '@mui/base';
+import { cx } from '@emotion/css';
 
-// import { cx } from '@emotion/css';
-// import generateUtilityClass from '@mui/material/generateUtilityClass';
-// import generateUtilityClasses from '@mui/material/generateUtilityClasses';
-// import { unstable_composeClasses as composeClasses } from '@mui/base';
-
-// export type WorkflowCardClasses = {
-//     root?: string;
-//     card?: string;
-// };
-
-// export type WorkflowCardClassKey = keyof WorkflowCardClasses;
-
-// export function getBrandedCardContainerUtilityClass(slot: string): string {
-//     return generateUtilityClass('BluiBrandedCardContainer', slot);
-// }
-
-// const workflowCardClasses: WorkflowCardClasses = generateUtilityClasses('BluiBrandedCardContainer', ['root', 'card']);
-
-type WorkflowCardBaseProps = {
+type WorkflowCardBaseProps = BoxProps & {
     loading?: boolean;
     backgroundImage?: string; // card background
     error?: boolean | string; // each screen should have an error state
+    cardStyles?: SxProps<Theme>;
+    CardProps?: CardPropsType;
+    LoaderComponent?: ReactNode;
+    classes?: BrandedCardContainerClasses;
+    slots?: { card?: React.ElementType; loader?: React.ElementType };
+    slotProps?: {
+        card?: CardPropsType;
+        loader?: SpinnerProps;
+    };
 };
 
-type WorkflowCardProps = CardProps &
+const useUtilityClasses = (ownerState: WorkflowCardBaseProps): Record<BrandedCardContainerClassKey, string> => {
+    const { classes } = ownerState;
+
+    const slots = {
+        root: ['root'],
+        card: ['card'],
+    };
+
+    return composeClasses(slots, getBrandedCardContainerUtilityClass, classes);
+};
+
+type WorkflowCardProps = CardPropsType &
     WorkflowCardHeaderProps &
     WorkflowCardInstructionProps &
     WorkflowCardActionsProps &
     WorkflowCardBaseProps;
 
-// const useUtilityClasses = (ownerState: WorkflowCardProps): Record<WorkflowCardClassKey, string> => {
-//     const { classes } = ownerState;
-
-//     const slots = {
-//         root: ['root'],
-//         card: ['card'],
-//     };
-
-//     return composeClasses(slots, getBrandedCardContainerUtilityClass, classes);
-// };
-
 export const WorkflowCard: React.FC<WorkflowCardProps> = (props) => {
-    const { loading, backgroundImage, error, children } = props;
+    const {
+        loading,
+        backgroundImage,
+        error,
+        children,
+        cardStyles,
+        CardProps,
+        slots = {},
+        slotProps = {},
+        LoaderComponent = <Spinner visible={loading} component={slots.loader} {...slotProps.loader} />,
+        classes = {},
+        className: userClassName,
+        ...otherProps
+    } = props;
     const theme = useTheme();
     const { background } = useInjectedUIContext();
-    // const defaultClasses = useUtilityClasses(props);
+    const defaultClasses = useUtilityClasses(props);
 
     return (
         <Box
@@ -67,8 +79,8 @@ export const WorkflowCard: React.FC<WorkflowCardProps> = (props) => {
                 justifyContent: 'center',
             }}
             style={background || {}}
-            // className={cx(defaultClasses.root, classes.root, userClassName)}
-            // {...otherProps}
+            className={cx(defaultClasses.root, classes.root, userClassName)}
+            {...otherProps}
         >
             <Card
                 sx={{
@@ -85,13 +97,14 @@ export const WorkflowCard: React.FC<WorkflowCardProps> = (props) => {
                         maxHeight: 'none',
                         borderRadius: 0,
                     },
-                    // ...cardStyles,
+                    ...cardStyles,
                 }}
-                // className={cx(defaultClasses.card, classes.card)}
-                // component={slots.card}
-                // {...CardProps}
-                // {...slotProps.card}
+                className={cx(defaultClasses.card, classes.card)}
+                component={slots.card}
+                {...CardProps}
+                {...slotProps.card}
             >
+                {LoaderComponent}
                 {children}
             </Card>
         </Box>
