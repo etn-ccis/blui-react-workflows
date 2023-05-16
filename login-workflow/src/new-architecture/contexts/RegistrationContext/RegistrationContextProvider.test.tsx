93391401f522fe5d8052d226833eaab5806430ac
daileytj <daileytj89@gmail.com>
2023-05-16 07:53:38 -0400

    Add more tests

--- login-workflow/src/new-architecture/contexts/RegistrationContext/RegistrationContextProvider.test.tsx
+++ login-workflow/src/new-architecture/contexts/RegistrationContext/RegistrationContextProvider.test.tsx
@@ -2,73 +2,22 @@ import React from 'react';
 import { render, cleanup, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { RegistrationContextProvider } from './provider';
-import i18next from 'i18next';
-import { dictionaries } from './dictionaries';
 import { useRegistrationContext } from './context';
 import { RegistrationContextProviderProps } from './types';
 import { renderHook } from '@testing-library/react-hooks';
+import { useTranslation } from 'react-i18next';
+import { i18nRegistrationInstance } from './i18nRegistrationInstance';
 
 afterEach(cleanup);
 
-const i18nRegistrationInstance = i18next.createInstance(
-    {
-        lng: 'en',
-        fallbackLng: 'en',
-        ns: ['bluiRegistration'],
-        defaultNS: 'bluiRegistration',
-        load: 'languageOnly',
-        detection: {
-            order: ['querystring', 'localStorage', 'navigator'],
-            caches: ['localStorage'],
-            lookupLocalStorage: 'blui-registration-i18nextLng',
-        },
-        react: { useSuspense: false },
-        interpolation: { escapeValue: false },
-        resources: {
-            en: {
-                bluiRegistration: {
-                    ...dictionaries.english.translation,
-                },
-            },
-            fr: {
-                bluiRegistration: {
-                    ...dictionaries.french.translation,
-                },
-            },
-            es: {
-                bluiRegistration: {
-                    ...dictionaries.spanish.translation,
-                },
-            },
-            zh: {
-                bluiRegistration: {
-                    ...dictionaries.chinese.translation,
-                },
-            },
-            pt: {
-                bluiRegistration: {
-                    ...dictionaries.portuguese.translation,
-                },
-            },
-        },
-    },
-    // We must provide a function as second parameter, otherwise i18next errors
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    (err, _t) => {
-        // @TODO: Handle this error state more graciously
-        // eslint-disable-next-line no-console
-        if (err) return console.log(err);
-    }
-);
+const defaultProps = {
+    language: 'en',
+    i18n: i18nRegistrationInstance,
+    navigate: (): void => {},
+    routeConfig: {},
+};
 
 describe('RegistrationContextProvider', () => {
-    const defaultProps = {
-        language: 'en',
-        i18n: i18nRegistrationInstance,
-        navigate: (): void => {},
-        routeConfig: {},
-    };
-
     it('should render RegistrationContextProvider without crashing', () => {
         render(<RegistrationContextProvider {...defaultProps}>Hello Registration</RegistrationContextProvider>);
 
@@ -141,5 +90,31 @@ describe('RegistrationContextProvider', () => {
 
         expect(screen.getByText('Child 1')).toBeInTheDocument();
         expect(screen.getByText('Child 2')).toBeInTheDocument();
+    });
+
+    it('should display correct language translations', async () => {
+        const RegisterComponent: React.FC<React.PropsWithChildren> = () => (
+            <RegistrationContextProvider {...defaultProps} />
+        );
+
+        const CustomFlow: React.FC = () => {
+            const Screen1: React.FC = () => {
+                const { t } = useTranslation();
+
+                // eslint-disable-next-line
+                renderHook((): RegistrationContextProviderProps => useRegistrationContext());
+                return <div>{t('bluiRegistration:REGISTRATION.EULA.LOADING')}</div>;
+            };
+
+            return (
+                <RegisterComponent>
+                    <Screen1 />
+                </RegisterComponent>
+            );
+        };
+
+        render(<CustomFlow />);
+        // eslint-disable-next-line
+        await ((): void => expect(screen.getByText('Loading End User License Agreement...')).toBeInTheDocument());
     });
 });
