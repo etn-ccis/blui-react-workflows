34ded78876865c11b5f1598d1862f251ccbff94b
Suraj Karambe <surajakarambe@eaton.com>
2023-05-23 20:25:21 +0530

    Cleaned up tests

--- login-workflow/src/new-architecture/components/RegistrationWorkflow/RegistrationWorkflow.test.tsx
+++ login-workflow/src/new-architecture/components/RegistrationWorkflow/RegistrationWorkflow.test.tsx
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom';
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { RegistrationWorkflow, RegistrationWorkflowProps } from './RegistrationWorkflow';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 afterEach(cleanup);
 
@@ -16,45 +17,45 @@ describe('RegistrationWorkflow', () => {
         expect(screen.getByText('Screen 1')).toBeInTheDocument();
     });
 
-    it('should render the default screens', () => {
-        render(
-            <RegistrationWorkflow
-                {...defaultProps}
-                children={[<Typography>Default 1</Typography>, <Typography>Default 2</Typography>]}
-            />
-        );
-        expect(screen.getByText('Default 1')).toBeInTheDocument();
-    });
-
-    it('should render the correct default screens, when initialScreenIndex prop is passed', () => {
-        render(
-            <RegistrationWorkflow
-                initialScreenIndex={1}
-                children={[<Typography>Indexed Default 1</Typography>, <Typography>Indexed Default 2</Typography>]}
-            />
-        );
-        expect(screen.queryByText('Indexed Default 1')).toBeNull();
-        expect(screen.getByText('Indexed Default 2')).toBeInTheDocument();
-    });
-
-    it('should render the custom screens', () => {
+    it('should render the multiple screens', () => {
         render(
             <RegistrationWorkflow initialScreenIndex={0}>
-                <Typography>Custom 1</Typography>
-                <Typography>Custom 2</Typography>
+                <Typography>Screen 1</Typography>
+                <Typography>Screen 2</Typography>
             </RegistrationWorkflow>
         );
-        expect(screen.getByText('Custom 1')).toBeInTheDocument();
+        expect(screen.getByText('Screen 1')).toBeInTheDocument();
     });
 
     it('should render the correct screen, when initialScreenIndex prop is passed', () => {
         render(
             <RegistrationWorkflow initialScreenIndex={1}>
-                <Typography>Indexed Custom 1</Typography>
-                <Typography>Indexed Custom 2</Typography>
+                <Typography>Indexed Screen 1</Typography>
+                <Typography>Indexed Screen 2</Typography>
             </RegistrationWorkflow>
         );
-        expect(screen.queryByText('Indexed Custom 1')).toBeNull();
-        expect(screen.getByText('Indexed Custom 2')).toBeInTheDocument();
+        expect(screen.queryByText('Indexed Screen 1')).toBeNull();
+        expect(screen.getByText('Indexed Screen 2')).toBeInTheDocument();
+    });
+
+    it('should call nextScreen function', () => {
+        const nextScreen = jest.fn();
+        const { getByText } = render(
+            <RegistrationWorkflow>
+                <>
+                    <Typography>Indexed Screen 1</Typography>
+                    <Button
+                        onClick={(): void => {
+                            nextScreen({ screenId: 'Eula', values: { accepted: true } });
+                        }}
+                    >
+                        Next
+                    </Button>
+                </>
+                <Typography>Indexed Screen 2</Typography>
+            </RegistrationWorkflow>
+        );
+        fireEvent.click(getByText('Next'));
+        expect(nextScreen).toHaveBeenCalledWith({ screenId: 'Eula', values: { accepted: true } });
     });
 });
