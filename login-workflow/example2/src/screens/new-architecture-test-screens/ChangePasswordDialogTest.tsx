83b179ac3d56c084cc03ff8881f69e98b61a2e8f
manojleaton <manojlokesh@eaton.com>
2023-06-06 15:27:28 +0530

    test commit

--- login-workflow/example2/src/screens/new-architecture-test-screens/ChangePasswordDialogTest.tsx
+++ login-workflow/example2/src/screens/new-architecture-test-screens/ChangePasswordDialogTest.tsx
@@ -2,5 +2,23 @@ import React from 'react';
 import { ChangePasswordDialogBase } from '@brightlayer-ui/react-auth-workflow';
 
 export const ChangePasswordDialogTest = () => {
-    return <ChangePasswordDialogBase open={true} onSubmit={(): void => {}} />;
+    return (
+        <ChangePasswordDialogBase
+            open={true}
+            onSubmit={(): void => {}}
+            PasswordProps={{
+                onPasswordChange: function (passwords: { password: string; confirm: string }): void {
+                    throw new Error('Function not implemented.');
+                },
+                newPasswordLabel: undefined,
+                initialNewPasswordValue: undefined,
+                confirmPasswordLabel: undefined,
+                initialConfirmPasswordValue: undefined,
+                passwordRequirements: undefined,
+                passwordRef: undefined,
+                confirmRef: undefined,
+                onSubmit: undefined,
+            }}
+        />
+    );
 };
