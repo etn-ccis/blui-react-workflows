bbf06dad5d69d57dd5d312324d69a3bb0ca43133
manojleaton <manojlokesh@eaton.com>
2023-06-09 12:51:52 +0530

    unit tests

---
+++ login-workflow/src/new-architecture/components/ChangePasswordDialog/ChangePasswordDialogBase.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { cleanup, render } from '@testing-library/react';
+import { ChangePasswordDialogBase } from './ChangePasswordDialogBase';
+
+afterEach(cleanup);
+
+describe('AccountDetailsScreenBase tests', () => {
+    it('renders without crashing', () => {
+        render(
+            <ChangePasswordDialogBase
+                open={true}
+                onSubmit={undefined}
+                PasswordProps={{
+                    onPasswordChange: function (passwords: { password: string; confirm: string }): void {
+                        throw new Error('Function not implemented.', passwords);
+                    },
+                    newPasswordLabel: '',
+                    initialNewPasswordValue: '',
+                    confirmPasswordLabel: '',
+                    initialConfirmPasswordValue: '',
+                    passwordRequirements: [],
+                    passwordRef: undefined,
+                    confirmRef: undefined,
+                    onSubmit: function (): void {
+                        throw new Error('Function not implemented.');
+                    },
+                }}
+                currentPasswordChange={function (currentPassword: string): void {
+                    throw new Error('Function not implemented.', currentPassword);
+                }}
+                enableButton={false}
+            />
+        );
+    });
+});
