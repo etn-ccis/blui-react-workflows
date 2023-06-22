1bf7dd899dd5b7aa8d592e9b9630d8206a1611a3
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-06-22 14:10:47 -0400

    remove react-hooks & comment

--- login-workflow/src/new-architecture/components/RegistrationWorkflow/RegistrationWorkflow.test.tsx
+++ login-workflow/src/new-architecture/components/RegistrationWorkflow/RegistrationWorkflow.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { cleanup, fireEvent, render, screen, renderHook, act } from '@testing-library/react';
-// import { act } from '@testing-library/react-hooks';
 import { RegistrationWorkflow, RegistrationWorkflowProps } from './RegistrationWorkflow';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
