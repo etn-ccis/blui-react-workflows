c6871583f9bb71c8a1eb42730003ad7ea6cbf0fd
daileytj <daileytj89@gmail.com>
2023-07-14 13:29:08 -0400

    Run prettier on CreatePasswordScreen

--- login-workflow/src/new-architecture/screens/CreatePasswordScreen/types.ts
+++ login-workflow/src/new-architecture/screens/CreatePasswordScreen/types.ts
@@ -2,4 +2,7 @@ import { SetPasswordProps } from '../../components';
 import { ErrorManagerProps } from '../../components/Error';
 import { WorkflowCardProps } from '../../components/WorkflowCard/WorkflowCard.types';
 
-export type CreatePasswordScreenProps = WorkflowCardProps & { PasswordProps?: SetPasswordProps, errorDisplayConfig?: ErrorManagerProps };
+export type CreatePasswordScreenProps = WorkflowCardProps & {
+    PasswordProps?: SetPasswordProps;
+    errorDisplayConfig?: ErrorManagerProps;
+};
