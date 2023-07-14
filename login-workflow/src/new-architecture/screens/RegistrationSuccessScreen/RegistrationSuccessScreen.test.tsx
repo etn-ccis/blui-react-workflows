52e14622e4c59dab7341663b747fbe338378fd4e
Suraj Karambe <surajakarambe@eaton.com>
2023-07-14 15:20:51 +0530

    Added test case for registration success screens

--- login-workflow/src/new-architecture/screens/RegistrationSuccessScreen/RegistrationSuccessScreen.test.tsx
+++ login-workflow/src/new-architecture/screens/RegistrationSuccessScreen/RegistrationSuccessScreen.test.tsx
@@ -1,26 +1,16 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, cleanup, screen } from '@testing-library/react';
+import { render, cleanup, screen, fireEvent, RenderResult } from '@testing-library/react';
 import { RegistrationSuccessScreen } from './RegistrationSuccessScreen';
 import {
     i18nRegistrationInstance,
     RegistrationContextProvider,
     RegistrationContextProviderProps,
-    RegistrationUIActions,
     RegistrationWorkflowContextProvider,
 } from '../../contexts';
+import { SuccessScreenProps } from '../SuccessScreen';
 
 afterEach(cleanup);
-
-const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
-
-export const registrationContextProviderProps = {
-    language: 'en',
-    i18n: i18nRegistrationInstance,
-    navigate: jest.fn(),
-    routeConfig: {},
-};
 
 const registrationWorkflowContextProps = {
     currentScreen: 0,
@@ -39,29 +29,55 @@ const registrationWorkflowContextProps = {
 };
 
 const registrationContextDefaultProps: RegistrationContextProviderProps = {
-    ...registrationContextProviderProps,
-    actions: (): RegistrationUIActions => ({
-        completeRegistration: async (
-            userData,
-            validationCode,
-            validationEmail
-        ): Promise<{ email: string; organizationName: string }> => {
-            await sleep(1000);
-
-            return { email: 'email@email.email', organizationName: 'organizationName' };
-        },
-    }),
+    language: 'en',
+    i18n: i18nRegistrationInstance,
+    navigate: jest.fn(),
+    routeConfig: {},
 };
 
 describe('RegistrationSuccessScreen', () => {
-    it('renders without crashing', () => {
+    let mockOnNext: any;
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    beforeEach(() => {
+        mockOnNext = jest.fn();
+    });
+
+    const renderer = (props?: SuccessScreenProps): RenderResult =>
         render(
             <RegistrationContextProvider {...registrationContextDefaultProps}>
                 <RegistrationWorkflowContextProvider {...registrationWorkflowContextProps}>
-                    <RegistrationSuccessScreen />
+                    <RegistrationSuccessScreen {...props} />
                 </RegistrationWorkflowContextProvider>
             </RegistrationContextProvider>
         );
+
+    it('renders without crashing', () => {
+        renderer();
+
         expect(screen.getByText('Account Created!')).toBeInTheDocument();
+    });
+
+    it('should display email id and organization name on success screen', () => {
+        renderer();
+
+        expect(screen.getByText('emailAddress@emailAddress.emailAddress')).toBeInTheDocument();
+        expect(screen.getByText('Acme Co.')).toBeInTheDocument();
+    });
+
+    it('should call onNext, when click on Continue button', () => {
+        renderer({
+            WorkflowCardActionsProps: {
+                onNext: mockOnNext(),
+            },
+        });
+
+        const continueButton = screen.getByText('Continue');
+        expect(continueButton).toBeInTheDocument();
+        fireEvent.click(continueButton);
+        expect(mockOnNext).toHaveBeenCalled();
     });
 });
