21dfe38af28f46654f82cbc6b0c41f9950047d71
daileytj <daileytj89@gmail.com>
2023-07-20 11:10:31 -0400

    Add more message box tests

--- login-workflow/src/new-architecture/components/Error/ErrorMessageBox.test.tsx
+++ login-workflow/src/new-architecture/components/Error/ErrorMessageBox.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, cleanup, screen } from '@testing-library/react';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
 import ErrorMessageBox from './ErrorMessageBox';
 
 afterEach(cleanup);
@@ -10,5 +10,20 @@ describe('ErrorMessageBox', () => {
         render(<ErrorMessageBox errorMessage="Error Message" />);
         const messageBox = screen.getByText('Error Message');
         expect(messageBox).toBeInTheDocument();
+    });
+
+    it('dismisses when the Close icon is clicked', () => {
+        const mockOnClose = jest.fn();
+        render(<ErrorMessageBox errorMessage="Error Message" onClose={mockOnClose} />);
+        const closeButton = screen.queryByTestId('error-message-box-close');
+        expect(closeButton).toBeInTheDocument();
+        fireEvent.click(closeButton);
+        expect(mockOnClose).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not render the Close icon when dismissible is false', () => {
+        render(<ErrorMessageBox errorMessage="Error Message" dismissible={false} />);
+        const closeButton = screen.queryByTestId('error-message-box-close');
+        expect(closeButton).not.toBeInTheDocument();
     });
 });
