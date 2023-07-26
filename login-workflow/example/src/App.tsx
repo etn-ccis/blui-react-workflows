4576f67d3e0373e1c5043e457d03a6b79ddd88e9
ektaghag-eaton <EktaNGhag@eaton.com>
2023-07-26 10:21:29 +0530

    Update app and home page

--- login-workflow/example/src/App.tsx
+++ login-workflow/example/src/App.tsx
@@ -3,26 +3,29 @@ import { AppContext } from './contexts/AppContextProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './screens';
 import { SecurityContextProvider } from '@brightlayer-ui/react-auth-workflow';
-import './translations/i18n';
+import { i18nAppInstance } from './translations/i18n';
+import { I18nextProvider } from 'react-i18next';
 
 export const App = (): JSX.Element => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [language, setLanguage] = useState('en');
 
     return (
-        <AppContext.Provider
-            value={{
-                isAuthenticated,
-                setIsAuthenticated,
-                language,
-                setLanguage,
-            }}
-        >
-            <BrowserRouter basename={'/'}>
-                <SecurityContextProvider>
-                    <AppRouter />
-                </SecurityContextProvider>
-            </BrowserRouter>
-        </AppContext.Provider>
+        <I18nextProvider i18n={i18nAppInstance} defaultNS={'blui'}>
+            <AppContext.Provider
+                value={{
+                    isAuthenticated,
+                    setIsAuthenticated,
+                    language,
+                    setLanguage,
+                }}
+            >
+                <BrowserRouter basename={'/'}>
+                    <SecurityContextProvider>
+                        <AppRouter />
+                    </SecurityContextProvider>
+                </BrowserRouter>
+            </AppContext.Provider>
+        </I18nextProvider>
     );
 };
