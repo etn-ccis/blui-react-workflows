ddd68ec43c38b863f27516de5cb29733553411aa
joebochill <boyle.p.joseph@gmail.com>
2023-07-27 14:53:43 -0400

    Merge divergent changes

--- login-workflow/example/src/App.tsx
+++ login-workflow/example/src/App.tsx
@@ -5,12 +5,32 @@ import { AppRouter } from './screens';
 import { I18nextProvider } from 'react-i18next';
 import { i18nAppInstance } from './translations/i18n';
 import { LocalStorage } from './store/local-storage';
-import { CircularProgress } from '@mui/material';
+import { Box, CircularProgress, SxProps, Theme } from '@mui/material';
+
+const containerStyles: SxProps<Theme> = {
+    width: '100%',
+    height: (t) => ({ xs: `calc(100vh - ${t.spacing(7)})`, md: `calc(100vh - ${t.spacing(8)})` }),
+    display: 'flex',
+    padding: 0,
+    overflow: 'auto',
+    position: 'relative',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+
+const emptyStateContainerStyles = {
+    margin: 'auto',
+    display: 'flex',
+    zIndex: 4,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
 
 export const App = (): JSX.Element => {
-    const appLanguage = window.localStorage.getItem('language');
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [language, setLanguage] = useState(appLanguage || 'en');
+    const [language, setLanguage] = useState(window.localStorage.getItem('language') ?? 'en');
     const [loginData, setLoginData] = useState<AppContextType['loginData']>({
         email: '',
         rememberMe: false,
@@ -18,12 +38,18 @@ export const App = (): JSX.Element => {
 
     const [isLoading, setIsLoading] = useState(true);
 
+    // handle language change
     useEffect(() => {
-        const rememberMeDetails = async (): Promise<void> => {
+        void i18nAppInstance.changeLanguage(language);
+    }, [language]);
+
+    // handle initialization of auth data on first load
+    useEffect(() => {
+        const initialize = async (): Promise<void> => {
             try {
-                const userList = await LocalStorage.readAuthData();
-                setLoginData({ email: userList.rememberMeData.user, rememberMe: userList.rememberMeData.rememberMe });
-                setIsAuthenticated(userList.userId ? true : false);
+                const userData = await LocalStorage.readAuthData();
+                setLoginData({ email: userData.rememberMeData.user, rememberMe: userData.rememberMeData.rememberMe });
+                setIsAuthenticated(Boolean(userData.userId));
             } catch (e) {
                 // handle any error state, rejected promises, etc..
             } finally {
@@ -31,12 +57,13 @@ export const App = (): JSX.Element => {
             }
         };
         // eslint-disable-next-line
-        rememberMeDetails();
-        void i18nAppInstance.changeLanguage(language);
+        initialize();
     }, []);
 
     return isLoading ? (
-        <CircularProgress size={70} variant={'indeterminate'} />
+        <Box sx={containerStyles}>
+            <CircularProgress sx={emptyStateContainerStyles} size={70} variant={'indeterminate'} />
+        </Box>
     ) : (
         <I18nextProvider i18n={i18nAppInstance} defaultNS={'blui'}>
             <AppContext.Provider
@@ -45,8 +72,6 @@ export const App = (): JSX.Element => {
                     onUserAuthenticated: (userData): void => {
                         setIsAuthenticated(true);
                         setLoginData(userData);
-                        // eslint-disable-next-line no-console
-                        console.log(userData);
                     },
                     onUserNotAuthenticated: (userData): void => {
                         setIsAuthenticated(false);
