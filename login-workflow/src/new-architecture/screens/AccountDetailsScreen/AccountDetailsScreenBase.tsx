178ec334cbe060249da3891f853c5c45b0eec187
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-08-01 12:55:57 -0400

    update handle change first & last name

--- login-workflow/src/new-architecture/screens/AccountDetailsScreen/AccountDetailsScreenBase.tsx
+++ login-workflow/src/new-architecture/screens/AccountDetailsScreen/AccountDetailsScreenBase.tsx
@@ -14,11 +14,11 @@ export const AccountDetailsScreenBase: React.FC<AccountDetailsScreenProps> = (pr
     const {
         firstNameLabel,
         initialFirstName,
-        firstNameValidator,
+        firstNameValidator = (): void => {},
         firstNameTextFieldProps,
         lastNameLabel,
         initialLastName,
-        lastNameValidator,
+        lastNameValidator = (): void => {},
         lastNameTextFieldProps,
         errorDisplayConfig,
     } = props;
@@ -34,95 +34,45 @@ export const AccountDetailsScreenBase: React.FC<AccountDetailsScreenProps> = (pr
     const [firstNameInput, setFirstNameInput] = React.useState(initialFirstName ? initialFirstName : '');
     const [lastNameInput, setLastNameInput] = React.useState(initialLastName ? initialLastName : '');
 
-    const [shouldValidateFirstName, setShouldValidateFirstName] = React.useState(false);
-    const [shouldValidateLastName, setShouldValidateLastName] = React.useState(false);
+    const [isFirstNameValid, setIsFirstNameValid] = React.useState(false);
+    const [isLastNameValid, setIsLastNameValid] = React.useState(false);
 
     const [firstNameError, setFirstNameError] = React.useState('');
     const [lastNameError, setLastNameError] = React.useState('');
 
-    const [showFirstNameError, setShowFirstNameError] = React.useState(false);
-    const [showLastNameError, setShowLastNameError] = React.useState(false);
+    const [shouldValidateFirstName, setShouldValidateFirstName] = React.useState(false);
+    const [shouldValidateLastName, setShouldValidateLastName] = React.useState(false);
 
-    const [isFirstNameValid, setIsFirstNameValid] = React.useState(false);
-    const [isLastNameValid, setIsLastNameValid] = React.useState(false);
-    const [isFormValid, setIsFormValid] = React.useState(false);
-
-    const validateFirstName = useCallback(
-        (firstName: string) => {
-            const validatorResponse = firstNameValidator(firstName);
-
-            setIsFirstNameValid(typeof validatorResponse === 'boolean' ? validatorResponse : false);
-            setShowFirstNameError(typeof validatorResponse === 'boolean' ? false : true);
-            setFirstNameError(typeof validatorResponse === 'string' ? validatorResponse : '');
-        },
-        [firstNameValidator, setIsFirstNameValid, setShowFirstNameError, setFirstNameError]
-    );
-
-    const validateLastName = useCallback(
-        (lastName: string) => {
-            const validatorResponse = lastNameValidator(lastName);
-
-            setIsLastNameValid(typeof validatorResponse === 'boolean' ? validatorResponse : false);
-            setShowLastNameError(typeof validatorResponse === 'boolean' ? false : true);
-            setLastNameError(typeof validatorResponse === 'string' ? validatorResponse : '');
-        },
-        [lastNameValidator, setIsLastNameValid, setShowLastNameError, setLastNameError]
-    );
-
-    const onFirstNameChange = useCallback(
+    const handleFirstNameInputChange = useCallback(
         (firstName: string) => {
             setFirstNameInput(firstName);
-            if (firstNameValidator) {
-                validateFirstName(firstName);
-            } else {
-                setIsFirstNameValid(true);
-            }
+            const firstNameValidatorResponse = firstNameValidator(firstName);
+
+            setIsFirstNameValid(typeof firstNameValidatorResponse === 'boolean' ? firstNameValidatorResponse : false);
+            setFirstNameError(typeof firstNameValidatorResponse === 'string' ? firstNameValidatorResponse : '');
         },
-        [firstNameValidator, validateFirstName]
+        [firstNameValidator]
     );
 
-    const onLastNameChange = useCallback(
+    const handleLastNameInputChange = useCallback(
         (lastName: string) => {
             setLastNameInput(lastName);
-            if (lastNameValidator) {
-                validateLastName(lastName);
-            } else {
-                setIsLastNameValid(true);
-            }
-        },
-        [lastNameValidator, validateLastName]
-    );
+            const lastNameValidatorResponse = lastNameValidator(lastName);
 
-    useEffect(() => {
-        if (firstNameInput && lastNameInput) {
-            if (firstNameValidator) {
-                validateFirstName(firstNameInput);
-            }
-            if (lastNameValidator) {
-                validateLastName(lastNameInput);
-            }
-            setIsFormValid(isFirstNameValid && isLastNameValid);
-        }
-    }, [
-        firstNameInput,
-        lastNameInput,
-        firstNameValidator,
-        lastNameValidator,
-        validateFirstName,
-        validateLastName,
-        setIsFormValid,
-        isFirstNameValid,
-        isLastNameValid,
-    ]);
+            setIsLastNameValid(typeof lastNameValidatorResponse === 'boolean' ? lastNameValidatorResponse : false);
+            setLastNameError(typeof lastNameValidatorResponse === 'string' ? lastNameValidatorResponse : '');
+        },
+        [lastNameValidator]
+    );
 
     useEffect(() => {
         if (firstNameInput.length > 0) {
             setShouldValidateFirstName(true);
-            onFirstNameChange(firstNameInput);
+            handleFirstNameInputChange(firstNameInput);
         }
         if (lastNameInput.length > 0) {
             setShouldValidateLastName(true);
-            onLastNameChange(lastNameInput);
+            handleLastNameInputChange(lastNameInput);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -137,18 +87,22 @@ export const AccountDetailsScreenBase: React.FC<AccountDetailsScreenProps> = (pr
                         id="first"
                         fullWidth
                         variant="filled"
-                        {...firstNameTextFieldProps}
                         inputRef={firstNameRef}
                         label={firstNameLabel}
                         value={firstNameInput}
-                        onChange={(e): void => onFirstNameChange(e.target.value)}
-                        onKeyUp={(e): void => {
-                            if (e.key === 'Enter' && lastNameRef.current) lastNameRef.current.focus();
-                        }}
-                        error={shouldValidateFirstName && showFirstNameError}
+                        error={shouldValidateFirstName && !isFirstNameValid}
                         helperText={shouldValidateFirstName && firstNameError}
                         sx={{
                             mb: { md: 0, sm: 1, xs: 4 },
+                        }}
+                        {...firstNameTextFieldProps}
+                        onChange={(e): void => {
+                            // eslint-disable-next-line no-unused-expressions
+                            firstNameTextFieldProps?.onChange && firstNameTextFieldProps.onChange(e);
+                            handleFirstNameInputChange(e.target.value);
+                        }}
+                        onKeyUp={(e): void => {
+                            if (e.key === 'Enter' && lastNameRef.current) lastNameRef.current.focus();
                         }}
                         onBlur={(): void => setShouldValidateFirstName(true)}
                     />
@@ -159,16 +113,21 @@ export const AccountDetailsScreenBase: React.FC<AccountDetailsScreenProps> = (pr
                         sx={{
                             mt: { md: 4, sm: 3 },
                         }}
-                        {...lastNameTextFieldProps}
                         inputRef={lastNameRef}
                         label={lastNameLabel}
                         value={lastNameInput}
-                        onChange={(e): void => onLastNameChange(e.target.value)}
-                        onKeyUp={(e): void => {
-                            if (e.key === 'Enter' && isFormValid && actionsProps.canGoNext) actionsProps.onNext();
-                        }}
-                        error={shouldValidateLastName && showLastNameError}
+                        error={shouldValidateLastName && !isLastNameValid}
                         helperText={shouldValidateLastName && lastNameError}
+                        {...lastNameTextFieldProps}
+                        onChange={(e): void => {
+                            // eslint-disable-next-line no-unused-expressions
+                            lastNameTextFieldProps?.onChange && lastNameTextFieldProps.onChange(e);
+                            handleLastNameInputChange(e.target.value);
+                        }}
+                        onKeyUp={(e): void => {
+                            if (e.key === 'Enter' && isFirstNameValid && isLastNameValid && actionsProps.canGoNext)
+                                actionsProps.onNext();
+                        }}
                         onBlur={(): void => setShouldValidateLastName(true)}
                     />
                 </ErrorManager>
