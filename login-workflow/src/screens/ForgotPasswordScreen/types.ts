f57fd1eb304f6a22525713c9b25a4a6b07638f2e
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-08-28 16:13:39 -0400

    revert prop types in forgot password screen

--- login-workflow/src/screens/ForgotPasswordScreen/types.ts
+++ login-workflow/src/screens/ForgotPasswordScreen/types.ts
@@ -3,6 +3,36 @@ import { WorkflowCardProps } from '../../components/WorkflowCard/WorkflowCard.ty
 import { SuccessScreenProps } from '../SuccessScreen';
 
 export type ForgotPasswordScreenProps = WorkflowCardProps & {
+    // used to display contact phone number
+    contactPhone?: string;
+
+    // used to display response time
+    responseTime?: string;
+
+    // used to update the instruction
+    description?: (responseTime: string) => React.ReactNode;
+
+    // used to update the screen title
+    title?: string;
+
+    // used to display back button
+    showBackButton?: boolean;
+
+    // used to display back button label
+    backButtonLabel?: string;
+
+    // used to enable to back button
+    canGoBack?: boolean | (() => boolean);
+
+    // used to display next button
+    showNextButton?: boolean;
+
+    // used to display next button label
+    nextButtonLabel?: string;
+
+    // used to enable to next button
+    canGoNext?: boolean | (() => boolean);
+
     /**
      * @param emailLabel The label for the email field
      * @returns string
