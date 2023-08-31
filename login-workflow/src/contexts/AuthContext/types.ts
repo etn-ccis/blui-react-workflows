e06c6f886892eec67c50221ead71715fcfb7fb91
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-08-31 11:53:58 -0400

    auth context updates

--- login-workflow/src/contexts/AuthContext/types.ts
+++ login-workflow/src/contexts/AuthContext/types.ts
@@ -8,11 +8,33 @@ import { RouteConfig } from '../../types';
 import { ErrorContextProviderProps } from '../ErrorContext/types';
 
 export type AuthContextProviderProps = {
+    /**
+     * Defines the API calls / functions to execute when certain actions are performed in the UI (such as pressing the Login button)
+     */
     actions: () => AuthUIActions;
+
+    /**
+     * Configures the language displayed on the screens
+     */
     language: string;
+
+    /**
+     * Function that can be called to navigate to a new route
+     * @param {string} url - path to route to
+     * @returns void
+     */
     navigate: (url: string) => void;
+
+    /**
+     * Object describing the URLs you are using for the relevant routes so the workflow can correctly navigate between screens
+     */
     routeConfig: RouteConfig;
+
+    /**
+     * An optional i18n object that is used to translate the UI. This is only needed if you want to use custom translation keys / languages inside any of the workflow screens
+     */
     i18n?: i18n;
+    
     rememberMeDetails?: {
         /**
          * Email address to show in the email field of Login after logout.
