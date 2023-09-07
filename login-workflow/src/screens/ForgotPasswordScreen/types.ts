7505f2e3d12ad9628eaef2610a2c80ce8ac96be6
manojleaton <manojlokesh@eaton.com>
2023-09-07 12:58:51 +0530

    type description updated

--- login-workflow/src/screens/ForgotPasswordScreen/types.ts
+++ login-workflow/src/screens/ForgotPasswordScreen/types.ts
@@ -3,37 +3,63 @@ import { WorkflowCardProps } from '../../components/WorkflowCard/WorkflowCard.ty
 import { SuccessScreenProps } from '../SuccessScreen';
 
 export type ForgotPasswordScreenProps = WorkflowCardProps & {
-    // label for the textfield
+    /**
+     * label for the textfield
+     */
     emailLabel?: string;
 
-    // used to pre-populate the email input field
+    /**
+     * used to pre-populate the email input field
+     */
     initialEmailValue?: string;
 
-    // used to test the input for valid formatting
+    /**
+     * used to test the input for valid formatting
+     * @param {string} email - validate format via EMAIL_REGEX
+     * @returns boolean | string
+     */
     emailValidator?: (email: string) => boolean | string;
 
-    // used for each slot in `ForgotPasswordScreenBase`
+    /**
+     * used for each slot in `ForgotPasswordScreenBase`
+     * @param {SuccessScreenProps} props - pass SuccessScreenProps to SuccessScreen
+     * @returns JSX.Element
+     */
     slots?: {
         SuccessScreen?: (props: SuccessScreenProps) => JSX.Element;
     };
 
-    // props applied to each slot
+    /**
+     * props applied to each slot
+     */
     slotProps?: {
         SuccessScreen?: SuccessScreenProps;
     };
 
-    // used to display contact phone number
+    /**
+     * used to display contact phone number
+     */
     contactPhone?: string;
 
-    // used to display response time
+    /**
+     * used to display response time
+     */
     responseTime?: string;
 
-    // used to update the instruction
+    /**
+     * used to update the instruction
+     * @param {string} responseTime - will add the response time in instructions through desciption function
+     * @returns React.ReactNode
+     */
     description?: (responseTime: string) => React.ReactNode;
 
-    // used to determine whether to show a success screen after the form is submitted
+    /**
+     * used to determine whether to show a success screen after the form is submitted
+     */
     showSuccessScreen?: boolean;
 
-    // used to configure how errors are rendered
+    /**
+     * used to configure how errors are rendered
+     */
     errorDisplayConfig?: ErrorManagerProps;
 };
