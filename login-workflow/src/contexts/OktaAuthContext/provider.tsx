d4a29ca62e693c47136bc382e2a3e92d93255582
Suraj Karambe <surajakarambe@eaton.com>
2024-08-24 00:39:03 +0530

    Fixed lint errors

--- login-workflow/src/contexts/OktaAuthContext/provider.tsx
+++ login-workflow/src/contexts/OktaAuthContext/provider.tsx
@@ -7,12 +7,11 @@ import React, { useEffect } from 'react';
 import { OktaAuthContextProviderProps } from './types';
 import { OktaAuthContext } from './context';
 import { I18nextProvider, useTranslation } from 'react-i18next';
-import { i18nOktaAuthInstance } from './i18nOktaAuthInstance';
 import { ErrorContext } from '../ErrorContext';
-import { OktaAuthDictionaries } from './OktaAuthDictionaries';
 import { AuthDictionaries } from '../AuthContext/AuthDictionaries';
 import { SharedDictionaries } from '../SharedDictionaries';
 import { ErrorManagerProps } from '../../components/Error/types';
+import { i18nAuthInstance } from '../AuthContext/i18nAuthInstance';
 
 const OktaAuthContextProviderContent: React.FC<
     React.PropsWithChildren<Omit<OktaAuthContextProviderProps, 'i18n'> & { PasswordDialog?: JSX.Element }>
@@ -40,24 +39,19 @@ const OktaAuthContextProviderContent: React.FC<
 export const OktaAuthContextProvider: React.FC<
     React.PropsWithChildren<OktaAuthContextProviderProps & { PasswordDialog?: JSX.Element }>
 > = (props) => {
-    const i18nInstance = props.i18n ?? i18nOktaAuthInstance;
+    const i18nInstance = props.i18n ?? i18nAuthInstance;
     const { language, i18n = i18nInstance, children, ...other } = props;
 
     if (props.i18n) {
         i18n.addResourceBundle('zh', 'bluiAuth', AuthDictionaries.chinese.translation, true, false);
-        i18n.addResourceBundle('zh', 'bluiOktaAuth', OktaAuthDictionaries.chinese.translation, true, false);
         i18n.addResourceBundle('zh', 'bluiCommon', SharedDictionaries.chinese.translation, true, false);
         i18n.addResourceBundle('zh', 'bluiAuth', AuthDictionaries.english.translation, true, false);
-        i18n.addResourceBundle('en', 'bluiOktaAuth', OktaAuthDictionaries.english.translation, true, false);
         i18n.addResourceBundle('en', 'bluiCommon', SharedDictionaries.english.translation, true, false);
         i18n.addResourceBundle('zh', 'bluiAuth', AuthDictionaries.french.translation, true, false);
-        i18n.addResourceBundle('fr', 'bluiOktaAuth', OktaAuthDictionaries.french.translation, true, false);
         i18n.addResourceBundle('fr', 'bluiCommon', SharedDictionaries.french.translation, true, false);
         i18n.addResourceBundle('zh', 'bluiAuth', AuthDictionaries.portuguese.translation, true, false);
-        i18n.addResourceBundle('pt', 'bluiOktaAuth', OktaAuthDictionaries.portuguese.translation, true, false);
         i18n.addResourceBundle('pt', 'bluiCommon', SharedDictionaries.portuguese.translation, true, false);
         i18n.addResourceBundle('zh', 'bluiAuth', AuthDictionaries.spanish.translation, true, false);
-        i18n.addResourceBundle('es', 'bluiOktaAuth', OktaAuthDictionaries.spanish.translation, true, false);
         i18n.addResourceBundle('es', 'bluiCommon', SharedDictionaries.spanish.translation, true, false);
     }
 
